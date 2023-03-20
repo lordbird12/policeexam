2909358182d037573aaa910d051706773ffd4c11
narongkorn-kevin <narongkorn.kevin@gmail.com>
2023-03-20 15:27:23 +0700

    update evniornment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
     // API_URL: 'https://asha-tech.co.th/police-api/public',
-    API_URL: 'https://api.policeexam.org',
+    // API_URL: 'https://api.policeexam.org',
+    API_URL: 'https://police-exam.dev-asha.com',
 };
