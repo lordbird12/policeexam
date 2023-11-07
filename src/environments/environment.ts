2736cbc0932e0946f643c09bdf40c33a09d4976e
Kevin <78962100+narongkorn-kevin@users.noreply.github.com>
2023-11-08 01:29:44 +0700

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,8 @@ export const environment = {
     production: true,
     // API_URL: 'https://asha-tech.co.th/police-api/public',
     // API_URL: 'https://api.policeexam.org',
-    API_URL: 'https://police-exam.dev-asha.com',
+    //API_URL: 'https://police-exam.dev-asha.com',
+     API_URL: 'https://api.policeexam.org',
 };
 
 /*
